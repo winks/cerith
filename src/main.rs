823059c1505cbf3288137a279774d7cda3c39cfc
Florian Anderiasch <fa@art-core.org>
2015-11-01 21:43:13 +0100

    Let there be rustfmt

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ use getopts::Options;
 use regex::Regex;
 use std::collections::HashSet;
 use std::env;
-use std::io::{Error,Read,Write};
+use std::io::{Error, Read, Write};
 use std::net::TcpStream;
 use std::thread;
 
 // General config stuff
 static VERSION: &'static str = "0.1.0";
-static NAME   : &'static str = "Cerith";
-static ADMIN  : &'static str = "wink!fhtagn@cordelia.art-core.org";
-static DEBUG  : bool = true;
+static NAME: &'static str = "Cerith";
+static ADMIN: &'static str = "wink!fhtagn@cordelia.art-core.org";
+static DEBUG: bool = true;
 
 // IRC config stuff
 static NICKNAME: &'static str = "Cerith";
@@ -25,17 +25,17 @@ static DEFAULT_PORT: i32 = 6667;
 static CTCP_DELIM: &'static str = "\x01";
 
 // Messages to be sent.
-static MSG_QUIT : &'static str = "Fin.";
+static MSG_QUIT: &'static str = "Fin.";
 static MSG_GREET: &'static str = "Hello World!";
 static MSG_IQUIT: &'static str = ":(";
-static MSG_NOPE : &'static str = "You're not the boss of me.";
+static MSG_NOPE: &'static str = "You're not the boss of me.";
 
 // Commands that are recognized
 static CMD_PREFIX: &'static str = "!";
 static CMD_QUIT: &'static str = "quit";
 static CMD_JOIN: &'static str = "join";
 static CMD_PART: &'static str = "part";
-static CMD_SAY : &'static str = "say";
+static CMD_SAY: &'static str = "say";
 static CMD_MODE: &'static str = "mode";
 
 struct User<'a> {
@@ -88,17 +88,17 @@ fn get_utc_time(msec: bool) -> String {
 
 fn get_local_time() -> String {
     let now = time::now_utc();
-    return format!("{}", now.rfc822()).replace(",","");
-    /*
-    //This would need %Z support in strftime on Windows :(
-    let now = time::now();
-    let fmt = "%a %d %b %Y %H:%M:%S %Z";
-    let result = match now.strftime(fmt) {
-        Ok(v) => v,
-        _     => now.rfc822(),
-    };
-    return format!("{}", result).replace(",","");
-    */
+    return format!("{}", now.rfc822()).replace(",", "");
+    //
+    // This would need %Z support in strftime on Windows :(
+    // let now = time::now();
+    // let fmt = "%a %d %b %Y %H:%M:%S %Z";
+    // let result = match now.strftime(fmt) {
+    // Ok(v) => v,
+    // _     => now.rfc822(),
+    // };
+    // return format!("{}", result).replace(",","");
+    //
 }
 
 fn parse_int(s: String) -> i32 {
@@ -119,7 +119,11 @@ fn parse_hostmask(s: &str) -> User {
         let ident = x.at(2).unwrap();
         let host = x.at(3).unwrap();
 
-        return User{nick:nick, ident:ident, host:host};
+        return User {
+            nick: nick,
+            ident: ident,
+            host: host,
+        };
     } else {
         panic!("Cannot parse host mask: {}", s);
     }
@@ -140,18 +144,20 @@ fn has_privilege(user: &User) -> bool {
     join_hostmask(&user) == ADMIN
 }
 
-// Adapted from https://github.com/mattnenterprise/rust-pop3/blob/20acb17197a7553d5a664725fe96df9fa5d042fd/src/pop3.rs
+// Adapted from https://github.com/mattnenterprise/
+// rust-pop3/blob/20acb17197a7553d5a664725fe96df9fa5d042fd/src/pop3.rs
 fn read_response(stream: &mut TcpStream) -> Vec<String> {
-    //Carriage return
+    // Carriage return
     let cr = 0x0d;
-    //Line Feed
+    // Line Feed
     let lf = 0x0a;
     let mut line_buffer: Vec<u8> = Vec::new();
     let mut all: Vec<String> = Vec::new();
-    while line_buffer.len() < 2 || (line_buffer[line_buffer.len()-1] != lf && line_buffer[line_buffer.len()-2] != cr) {
+    while line_buffer.len() < 2 ||
+          (line_buffer[line_buffer.len() - 1] != lf && line_buffer[line_buffer.len() - 2] != cr) {
         let byte_buffer: &mut [u8] = &mut [0];
         match stream.read(byte_buffer) {
-            Ok(_) => {},
+            Ok(_) => {}
             Err(_) => println!("Error Reading!"),
         }
         line_buffer.push(byte_buffer[0]);
@@ -159,11 +165,11 @@ fn read_response(stream: &mut TcpStream) -> Vec<String> {
 
     match String::from_utf8(line_buffer.clone()) {
         Ok(res) => {
-            //println!("DEBUG {:?}", res);
+            // println!("DEBUG {:?}", res);
             all.push(res);
-            //line_buffer = Vec::new();
-        },
-        Err(_) => panic!("Failed to read the response")
+            // line_buffer = Vec::new();
+        }
+        Err(_) => panic!("Failed to read the response"),
     }
 
     all
@@ -221,7 +227,8 @@ fn send_privmsg(stream: &mut TcpStream, to: &str, msg: &str) {
 }
 
 fn send_user(stream: &mut TcpStream, name: &str, mode: i32, realname: &str) {
-    let _ = send_raw(stream, &(format!("USER {} {} * :{}\n", name, mode, realname)));
+    let _ = send_raw(stream,
+                     &(format!("USER {} {} * :{}\n", name, mode, realname)));
 }
 
 fn send_quit(stream: &mut TcpStream, msg: &str) {
@@ -240,13 +247,13 @@ fn handle_line(stream: &mut TcpStream, line: &str) -> Event {
     let event_cmd_join = format!("{}{}\\s+(.*)", CMD_PREFIX, CMD_JOIN);
     let event_cmd_part = format!("{}{}\\s+(\\S+)(\\s+)?(.*)?", CMD_PREFIX, CMD_PART);
     let event_cmd_quit = format!("{}{}(\\s+)?(.*)?", CMD_PREFIX, CMD_QUIT);
-    let event_cmd_say  = format!("{}{}\\s+(\\S+)\\s+(.+)", CMD_PREFIX, CMD_SAY);
+    let event_cmd_say = format!("{}{}\\s+(\\S+)\\s+(.+)", CMD_PREFIX, CMD_SAY);
     let event_cmd_mode = format!("{}{}\\s+(\\S+)\\s+(.+)", CMD_PREFIX, CMD_MODE);
 
     let re_cmd_join = Regex::new(&event_cmd_join[..]).unwrap();
     let re_cmd_part = Regex::new(&event_cmd_part[..]).unwrap();
     let re_cmd_quit = Regex::new(&event_cmd_quit[..]).unwrap();
-    let re_cmd_say  = Regex::new(&event_cmd_say[..]).unwrap();
+    let re_cmd_say = Regex::new(&event_cmd_say[..]).unwrap();
     let re_cmd_mode = Regex::new(&event_cmd_mode[..]).unwrap();
 
     if re_motd.is_match(line) {
@@ -263,10 +270,10 @@ fn handle_line(stream: &mut TcpStream, line: &str) -> Event {
 
         return Event::PingPong;
     } else if re_priv.is_match(line) {
-        let caps   = re_priv.captures(line).unwrap();
+        let caps = re_priv.captures(line).unwrap();
         let sender = caps.at(1).unwrap();
-        let msg    = caps.at(2).unwrap_or("");
-        let user   = parse_hostmask(sender);
+        let msg = caps.at(2).unwrap_or("");
+        let user = parse_hostmask(sender);
 
         if msg.len() < 1 {
             debug(format!("PRIVMSG TOO SHORT {}", sender));
@@ -275,7 +282,7 @@ fn handle_line(stream: &mut TcpStream, line: &str) -> Event {
 
         // these are the bot commands with prefix
         if msg.chars().nth(0) == CMD_PREFIX.chars().nth(0) {
-            if ! has_privilege(&user) {
+            if !has_privilege(&user) {
                 send_privmsg(stream, user.nick, MSG_NOPE);
 
                 return Event::Unprivileged;
@@ -289,25 +296,26 @@ fn handle_line(stream: &mut TcpStream, line: &str) -> Event {
                 return Event::Quit(quit_msg.to_string());
             } else if is_command(msg, CMD_JOIN) {
                 let caps_cmd = re_cmd_join.captures(msg).unwrap();
-                let channel  = caps_cmd.at(1).unwrap();
+                let channel = caps_cmd.at(1).unwrap();
                 debug(format!("JOIN {}|{}|{}", sender, msg, channel));
                 send_join(stream, channel);
             } else if is_command(msg, CMD_PART) {
                 let caps_cmd = re_cmd_part.captures(msg).unwrap();
-                let channel  = caps_cmd.at(1).unwrap();
+                let channel = caps_cmd.at(1).unwrap();
                 let part_msg = caps_cmd.at(3).unwrap_or("");
                 debug(format!("PART {}|{}|{}|{}", sender, msg, channel, part_msg));
                 send_part(stream, channel, part_msg);
             } else if is_command(msg, CMD_MODE) {
                 let caps_cmd = re_cmd_mode.captures(msg).unwrap();
-                let channel  = caps_cmd.at(1).unwrap();
-                let rest     = caps_cmd.at(2).unwrap_or("");
+                let channel = caps_cmd.at(1).unwrap();
+                let rest = caps_cmd.at(2).unwrap_or("");
 
                 if rest.len() < 2 {
-                    let lists: HashSet<_> = [
-                        "I", // invitations masks
-                        "e", // exemptions masks
-                    ].iter().cloned().collect();
+                    let lists: HashSet<_> = ["I" /* invitations masks */,
+                                             "e" /* exemptions masks */]
+                                                .iter()
+                                                .cloned()
+                                                .collect();
 
                     if rest.len() == 1 && lists.contains(rest) {
                         debug(format!("CHANMODE L {}|{}|{}|{}", sender, msg, channel, rest));
@@ -327,27 +335,25 @@ fn handle_line(stream: &mut TcpStream, line: &str) -> Event {
                 }
 
                 // https://www.alien.net.au/irc/chanmodes.html
-                let arity_0: HashSet<_> = [
-                    "c", // no colors
-                    "C", // no ctcp
-                    "m", // moderated
-                    "n", // no external messages
-                    "r", // registered users only
-                    "R", // registered users only
-                    "s", // secret
-                    "S", // strip colors
-                    "t", // topic lock
-                    "z", // secure joins only
-                ].iter().cloned().collect();
+                let arity_0: HashSet<_> = ["c" /* no colors */, "C" /* no ctcp */,
+                                           "m" /* moderated */,
+                                           "n" /* no external messages */,
+                                           "r" /* registered users only */,
+                                           "R" /* registered users only */,
+                                           "s" /* secret */, "S" /* strip colors */,
+                                           "t" /* topic lock */,
+                                           "z" /* secure joins only */]
+                                              .iter()
+                                              .cloned()
+                                              .collect();
 
-                let arity_1: HashSet<_> = [
-                    "b", // ban
-                    "h", // half-op
-                    "k", // channel key
-                    "l", // channel limit
-                    "o", // operator
-                    "v", // voice
-                ].iter().cloned().collect();
+                let arity_1: HashSet<_> = ["b" /* ban */, "h" /* half-op */,
+                                           "k" /* channel key */,
+                                           "l" /* channel limit */, "o" /* operator */,
+                                           "v" /* voice */]
+                                              .iter()
+                                              .cloned()
+                                              .collect();
 
                 if arity_0.contains(second) {
                     let mode = &rest[0..2];
@@ -363,13 +369,13 @@ fn handle_line(stream: &mut TcpStream, line: &str) -> Event {
                     send_mode(stream, channel, &rest[..]);
                 } else {
                     debug(format!("CHANMODE ? {}|{}|{}", sender, msg, channel));
-                    //send_privmsg(stream, channel, &say_msg2[..]);
+                    // send_privmsg(stream, channel, &say_msg2[..]);
                     return Event::CommandCancelled;
                 }
             } else if is_command(msg, CMD_SAY) {
                 let caps_cmd = re_cmd_say.captures(msg).unwrap();
-                let channel  = caps_cmd.at(1).unwrap();
-                let say_msg  = caps_cmd.at(2).unwrap_or("");
+                let channel = caps_cmd.at(1).unwrap();
+                let say_msg = caps_cmd.at(2).unwrap_or("");
                 if say_msg.len() > 0 {
                     if say_msg.len() > 4 && &say_msg[0..4] == "/me " {
                         let ctcp = "ACTION";
@@ -390,11 +396,11 @@ fn handle_line(stream: &mut TcpStream, line: &str) -> Event {
         } else if &msg[0..1] == "\x01" {
             // CTCP stuff
             let len = msg.len();
-            if len < 3 || &msg[len-1..len] != "\x01" {
+            if len < 3 || &msg[len - 1..len] != "\x01" {
                 debug(format!("PRIVMSG CTCP:FAIL {}|{}", sender, msg));
                 return Event::PrivMsg;
             }
-            let payload = &msg[1..len-1];
+            let payload = &msg[1..len - 1];
             debug(format!("PRIVMSG CTCP:OK {}|{}", sender, payload));
 
             if payload == "VERSION" {
@@ -415,7 +421,7 @@ fn handle_line(stream: &mut TcpStream, line: &str) -> Event {
                 if payload.len() < 6 {
                     return Event::Unknown;
                 }
-                //let rest = &payload[5..];
+                // let rest = &payload[5..];
                 let command = "PING";
 
                 let reply = get_utc_time(true);
@@ -456,10 +462,10 @@ fn connect(host: &str, port: i32) {
             Event::Quit(v) => {
                 debug(format!("Event::Quit {}", v));
                 quit_msg = v;
-                break
-            },
+                break;
+            }
             Event::Connected => debug(format!("Event::Connected")),
-            _ => ()
+            _ => (),
         }
     }
 
@@ -481,8 +487,12 @@ fn main() {
     opts.optopt("p", "port", "Autoconnect port", "PORT");
 
     let matches = match opts.parse(&args[1..]) {
-        Ok(m)  => { m }
-        Err(f) => { panic!(f.to_string()) }
+        Ok(m) => {
+            m
+        }
+        Err(f) => {
+            panic!(f.to_string())
+        }
     };
     if matches.opt_present("h") {
         print_usage(&program, opts);
@@ -494,7 +504,9 @@ fn main() {
     }
     let server = match matches.opt_str("c") {
         Some(s) => s,
-        None => { panic!("No server given.")}
+        None => {
+            panic!("No server given.")
+        }
     };
     let mut port = match matches.opt_str("p") {
         Some(s) => parse_int(s),
